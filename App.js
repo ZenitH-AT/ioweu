f1519fc5278ea888e003fe016627254d81280540
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2019-11-22 02:01:10 +0200

    Updated packages and supress timing warnings

--- App.js
+++ App.js
@@ -59,3 +59,17 @@ export default createAppContainer(
 );
 
 /* --- Navigation - END --- */
+
+//Remove timer warnings
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+
+const _console = _.clone(console);
+
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
